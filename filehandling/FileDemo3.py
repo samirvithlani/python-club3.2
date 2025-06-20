cdb9fd8844db3c69481317bc31efe97580ced992
samirvithlani <samir.vithlani83955@gmail.com>
2025-06-20 13:20:32 +0530

    file

---
+++ filehandling/FileDemo3.py
@@ -0,0 +1,22 @@
+# with open("./filehandling/exp1.txt","r")as file:
+#     data = file.read()
+#     print(f"{data}")
+    
+# with open("./filehandling/exp1.txt","r")as file:
+#     #loop
+#     data = file.readline()
+#     print(f"{data.strip()}")
+        
+# with open("./filehandling/exp1.txt","r")as file:
+#     data = file.readlines()
+#     print(f"{data}")
+
+
+# with open("./filehandling/exp1.txt","r")as file:
+#     for data in file:
+#         print(data,end=" ")
+
+# with open("./filehandling/exp1.txt","r")as file:
+#     data = file.read(10)
+#     print(f"{data}")
+                
